0d3569e8d9dd58f4d5eba8a4c2dd1dec0c3766e9
sni424 <79966260+sni424@users.noreply.github.com>
2022-02-19 06:21:26 +0900

    Add files via upload

---
+++ src/reply.js/Creply.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, useState } from "react";
+import useFetch from "../hooks/useFetch";
+
+function Creply({ setid }) {
+
+    const [nContents, setnContents] = useState("");
+
+    function onChange(e) {
+        const { value } = e.target;
+        setnContents(value);
+        console.log(value);
+    }
+
+    function CreateReply(event) {
+        event.preventDefault();
+        fetch(`http://localhost:3001/boardsreply`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    sameId: parseInt(setid),
+                    comment: nContents,
+                    like: 0
+                }),
+            }).then(res => {
+                if (res.ok) {
+                    setnContents("");
+                    alert("생성이 완료되었습니다.");
+                }
+            })
+    };
+    return (
+        <div>
+            <form onSubmit={CreateReply}>
+                <input type="text" placeholder="댓글" value={nContents} onChange={onChange}></input>
+                <button >입력</button>
+            </form>
+        </div>
+    );
+};
+
+export default Creply;
