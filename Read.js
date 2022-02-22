a24c67bb16cfa00e9c55f45c8f8d13395cd3609c
sni424 <79966260+sni424@users.noreply.github.com>
2022-02-22 23:03:18 +0900

    Add files via upload

--- Read.js
+++ Read.js
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import Creply from "../reply/Creply";
+import ReplyShow from "../reply/ReplyShow";
 
 function Read() {
     const data = useFetch("http://localhost:3001/boards");
+    const nReply = useFetch("http://localhost:3001/boardsreply");
 
     let { setid } = useParams();
     let [put, setPut] = useState(`/putndelete/${setid}`);
     const navi = useNavigate();
+    const [checkUseEffect, setCheckUseEffect] = useState(true);
 
-    function removeText(e) {
+
+    function removeText() {
         if (data.length > 0) {
             if (window.confirm("삭제 하시겠습니까?")) {
                 fetch(`http://localhost:3001/boards/${parseInt(setid)}`,
@@ -26,8 +31,9 @@ function Read() {
     }
 
     useEffect(() => {
+        console.log("hello");
+    }, [data.title, data.content, checkUseEffect])
 
-    }, [data.title, data.content])
 
     return (
         <div>
@@ -45,8 +51,15 @@ function Read() {
                 <button className="submit-button" type="submit">수정</button>
             </Link>
             <button className="submit-button" type="submit" onClick={removeText}>삭제</button><br></br>
+            <Creply setid={setid}></Creply>
+            {nReply.length > 0 &&
+                nReply.map((a, i) => {
+                    return <ReplyShow nReply={nReply} setCheckUseEffect={setCheckUseEffect} checkUseEffect={checkUseEffect} setid={setid} key={i} index={i} sameId={a.sameId} comment={a.comment} newid={a.id} like={a.like}></ReplyShow>
+                })
+            }
         </div>
     );
 };
 
 export default Read;
+
